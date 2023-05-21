d3c8eaf0e7cb05d98237c005399e8b1fb3af2c00
AlexAndriushchenko <AlexAndriushchenko@users.noreply.github.com>
2023-05-21 15:48:00 +0000

    deploy: 61096b639eab2ee77b3c98d11623ba5fc947e3d1

---
+++ assets/Home-5ca31a3f.js
@@ -0,0 +1,7 @@
+import{s as t,j as e}from"./index-a45f7633.js";const s=t.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 150px;
+`,o=()=>e.jsx(s,{children:e.jsx("h1",{children:"Welcome to Tweets"})});export{o as default};
--- assets/Home-e83a66ab.js
+++
@@ -1,7 +0,0 @@
-import{s as t,j as e}from"./index-54a3aaad.js";const s=t.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin-top: 150px;
-`,o=()=>e.jsx(s,{children:e.jsx("h1",{children:"Wellcome to Tweets"})});export{o as default};
