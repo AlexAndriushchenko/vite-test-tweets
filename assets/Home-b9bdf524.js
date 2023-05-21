b2e1999c7817e13feeb6215c7e98c7a5bc822f7f
AlexAndriushchenko <AlexAndriushchenko@users.noreply.github.com>
2023-05-21 17:53:32 +0000

    deploy: 3a11dacf386d6c91b7b2617b5f10b340164b0c9b

--- assets/Home-b9bdf524.js
+++ assets/Home-b9bdf524.js
@@ -1,7 +1,8 @@
-import{s as t,j as e}from"./index-a45f7633.js";const s=t.div`
+import{s as t,j as e}from"./index-8b8b2504.js";const s=t.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   margin-top: 150px;
+  font-size: 28px;
 `,o=()=>e.jsx(s,{children:e.jsx("h1",{children:"Welcome to Tweets"})});export{o as default};
